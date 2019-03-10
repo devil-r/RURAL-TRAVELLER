2b4798691d1e0aa0ff0ddc8790f9eb675d623131
tushar dubey <dtushar279@gmail.com>
2019-03-11 02:13:40 +0530

    explore

--- my app/app/src/main/java/com/example/souhardkataria/ruralt/userExploreFragment.java
+++ my app/app/src/main/java/com/example/souhardkataria/ruralt/userExploreFragment.java
@@ -1,8 +1,11 @@
 package com.example.souhardkataria.ruralt;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +14,8 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class userExploreFragment extends Fragment {
-
+public class userExploreFragment extends Fragment implements View.OnClickListener {
+    CardView c1,c2,c3;
 
     public userExploreFragment() {
         // Required empty public constructor
@@ -23,7 +26,32 @@ public class userExploreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_explore, container, false);
+        View view=inflater.inflate(R.layout.fragment_user_explore, container, false);
+        c1=(CardView) view.findViewById(R.id.card1);
+        c2=(CardView) view.findViewById(R.id.card2);
+        c3=(CardView) view.findViewById(R.id.card3);
+        c1.setOnClickListener(this);c2.setOnClickListener(this);c3.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.card1:
+                Intent intent1=new Intent(getActivity(),packages.class);
+                intent1.putExtra("Village","Landour");
+                startActivity(intent1);
+                break;
+            case R.id.card2:
+                Intent intent2=new Intent(getActivity(),packages.class);
+                intent2.putExtra("Village","Malana");
+                startActivity(intent2);
+                break;
+            case R.id.card3:
+                Intent intent3=new Intent(getActivity(),packages.class);
+                intent3.putExtra("Village","Diskit");
+                startActivity(intent3);
+                break;
+        }
+    }
 }
