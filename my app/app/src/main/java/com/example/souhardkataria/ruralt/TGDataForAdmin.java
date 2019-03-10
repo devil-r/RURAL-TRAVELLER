eaa8f30f9acf206cee0b8bb6bbfc3c0576899ef1
DodiyaParth <parthdodiya999@gmail.com>
2019-03-10 17:26:14 +0530

    Admin Dashboard

---
+++ my app/app/src/main/java/com/example/souhardkataria/ruralt/TGDataForAdmin.java
@@ -0,0 +1,13 @@
+package com.example.souhardkataria.ruralt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TGDataForAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tgdata_for_admin);
+    }
+}
