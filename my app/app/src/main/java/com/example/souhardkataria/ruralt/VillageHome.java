fad1a774cf17d5af9257dd4f640504475a08ac36
SouhardK <souhard.kataria@gmail.com>
2019-03-10 18:29:33 +0530

    maps

--- my app/app/src/main/java/com/example/souhardkataria/ruralt/VillageHome.java
+++ my app/app/src/main/java/com/example/souhardkataria/ruralt/VillageHome.java
@@ -1,10 +1,18 @@
 package com.example.souhardkataria.ruralt;
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class VillageHome extends AppCompatActivity {
@@ -21,16 +29,35 @@ public class VillageHome extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-       Intent intent = getIntent();
-       str = intent.getStringExtra("Village");
-        //Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
-       //getSupportActionBar().setTitle(str);
+        Intent intent = getIntent();
+        str = intent.getStringExtra("Village");
+
+        //getSupportActionBar().setTitle(str);
 
 
         setContentView(R.layout.activity_village_home);
         viewPager = findViewById(R.id.VillageHomePager);
         PagerAdapt adapt = new PagerAdapt(getSupportFragmentManager());
         viewPager.setAdapter(adapt);
+
+        Button maps  = ( Button )findViewById(R.id.map_button);
+        maps.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(VillageHome.this, MapsActivity.class);
+
+                try {
+                    startActivity(intent);
+                }catch (Exception e)
+                {
+                    Toast.makeText(VillageHome.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Log.d("Mapsdisplay",e.getMessage());
+                }
+            }
+
+
+        });
 
 
     }
@@ -44,7 +71,7 @@ public class VillageHome extends AppCompatActivity {
         {
             case R.id.StayButton:
                 Intent intent = new Intent(this,StayActivity.class);
-                intent.putExtra("Village",str);
+                //  intent.putExtra("Village",str);
                 startActivity(intent);
                 break;
             case R.id.ButtonDecr:
@@ -53,10 +80,9 @@ public class VillageHome extends AppCompatActivity {
                 Toast.makeText(this, "Stay", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.ButtonPackage:
-                Intent intent1 = new Intent(this,packages.class);
-                intent1.putExtra("Village",str);
-                 startActivity(intent1);
-                 break;
+
+                startActivity(new Intent(this,packages.class));
+                break;
 
             case R.id.Eat:
                 Intent i=new Intent(this, Eatactivity.class);
@@ -66,9 +92,8 @@ public class VillageHome extends AppCompatActivity {
             case R.id.ButtonVisit:
                 startActivity(new Intent(this,Visitactivity.class));
                 break;
-        }
-    }
 
-    public static class trying {
+
+        }
     }
 }
