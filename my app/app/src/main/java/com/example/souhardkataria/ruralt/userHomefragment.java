70023c9705fcfa19f9100f176cee32cabbc8eb1c
DodiyaParth <parthdodiya999@gmail.com>
2019-03-11 00:35:11 +0530

    12:30 AM  commit

--- my app/app/src/main/java/com/example/souhardkataria/ruralt/userHomefragment.java
+++ my app/app/src/main/java/com/example/souhardkataria/ruralt/userHomefragment.java
@@ -6,14 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class userHomefragment extends Fragment {
-
-
+    FirebaseAuth mAuth;
+    TextView name;
     public userHomefragment() {
         // Required empty public constructor
     }
@@ -23,7 +27,18 @@ public class userHomefragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_homefragment, container, false);
+        View view=inflater.inflate(R.layout.fragment_user_homefragment, container, false);
+        name=view.findViewById(R.id.nameString);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String email="Hi, Human";
+        if (user != null) {
+            email = user.getEmail().toString();
+            String uid = user.getUid();
+
+        }
+        name.setText("Hi, "+email);
+        return view;
     }
 
 }
