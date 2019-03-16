ce8cb5295ecf452006375c622a4acb1304183e31
tushar dubey <dtushar279@gmail.com>
2019-03-17 02:45:11 +0530

    wallet with addition issue

--- my app/app/src/main/java/com/example/souhardkataria/ruralt/wallet.java
+++ my app/app/src/main/java/com/example/souhardkataria/ruralt/wallet.java
@@ -1,51 +1,64 @@
 package com.example.souhardkataria.ruralt;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class wallet extends AppCompatActivity {
     FloatingActionButton fab;
-    String m_text,c_bal;
+    String add_bal,c_bal;
+    DatabaseReference f=FirebaseDatabase.getInstance().getReference("wallet");
+    final Context c= this;
     TextView mybal;
+    int x=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
         fab=findViewById(R.id.fab);
         mybal=findViewById(R.id.check_balance);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                opendialog();
+                LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
+                View mView = layoutInflaterAndroid.inflate(R.layout.user_input_dialog_box, null);
+                AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
+                alertDialogBuilderUserInput.setView(mView);
+
+                final EditText userInputDialogEditText = (EditText) mView.findViewById(R.id.userInputDialog);
+                alertDialogBuilderUserInput.setCancelable(false);
+                alertDialogBuilderUserInput.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogBox, int id) {
+                        // ToDo get user input here
+                        String y = userInputDialogEditText.getText().toString();
+                        int xx = Integer.parseInt(y);
+                        x = x+xx;
+                        mybal.setText(x);
+                    }
+                });
+                alertDialogBuilderUserInput.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                dialogBox.cancel();
+                            }
+                        });
+
+                AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+                alertDialogAndroid.show();
             }
         });
     }
-    public void opendialog()
-    {
-        android.app.AlertDialog.Builder builder=new AlertDialog.Builder(this);
-        builder.setTitle("Add Money");
-        final EditText input = new EditText(this);
-        input.setInputType(InputType.TYPE_NUMBER_VARIATION_NORMAL);
-        builder.setView(input);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                m_text = input.getText().toString();
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        builder.show();
-    }
+
 }
