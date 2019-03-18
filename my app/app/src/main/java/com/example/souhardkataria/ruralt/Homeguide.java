5e3ca2b6467e356539cb6e21ed31dd44e1c23979
tushar dubey <dtushar279@gmail.com>
2019-03-18 13:06:24 +0530

    some changes

--- my app/app/src/main/java/com/example/souhardkataria/ruralt/Homeguide.java
+++ my app/app/src/main/java/com/example/souhardkataria/ruralt/Homeguide.java
@@ -1,8 +1,10 @@
 package com.example.souhardkataria.ruralt;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +15,7 @@ import android.view.ViewGroup;
  */
 public class Homeguide extends Fragment {
 
-
+    CardView c;
     public Homeguide() {
         // Required empty public constructor
     }
@@ -23,7 +25,17 @@ public class Homeguide extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_homeguide, container, false);
+        View view =inflater.inflate(R.layout.fragment_homeguide, container, false);
+        c=view.findViewById(R.id.card);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(getActivity(),packages.class);
+                intent1.putExtra("Village","Landour");
+                startActivity(intent1);
+            }
+        });
+        return  view;
     }
 
 }
