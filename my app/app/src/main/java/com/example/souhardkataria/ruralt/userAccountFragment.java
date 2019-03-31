e25ba37a1566bf826f1e28a0bd4eb3fbcb6f3027
SouhardK <souhard.kataria@gmail.com>
2019-04-01 01:54:53 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	my app/.idea/assetWizardSettings.xml
    #	my app/.idea/caches/gradle_models.ser

--- my app/app/src/main/java/com/example/souhardkataria/ruralt/userAccountFragment.java
+++ my app/app/src/main/java/com/example/souhardkataria/ruralt/userAccountFragment.java
@@ -3,11 +3,20 @@ package com.example.souhardkataria.ruralt;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -19,8 +28,9 @@ public class userAccountFragment extends Fragment {
     public userAccountFragment() {
         // Required empty public constructor
     }
-
-
+    String old;
+    DatabaseReference Wallet= FirebaseDatabase.getInstance().getReference("Wallet");
+    String uid= FirebaseAuth.getInstance().getCurrentUser().getUid();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -89,7 +99,9 @@ public class userAccountFragment extends Fragment {
         wallet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getActivity(),wallet.class));
+
+                Intent I=new Intent(getActivity(),wallet.class);
+                startActivity(I);
             }
         });
         Button Forum = view.findViewById(R.id.forum);
