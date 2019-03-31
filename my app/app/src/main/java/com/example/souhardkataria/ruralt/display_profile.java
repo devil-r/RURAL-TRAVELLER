19d2745786e8f1b2a3cb9397024a942f5d1d6998
akshitpatel01 <akshitpatel01@gmail.com>
2019-04-01 02:53:18 +0530

    pre final

--- my app/app/src/main/java/com/example/souhardkataria/ruralt/display_profile.java
+++ my app/app/src/main/java/com/example/souhardkataria/ruralt/display_profile.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,16 +21,18 @@ import com.google.firebase.database.ValueEventListener;
 
 public class display_profile extends AppCompatActivity {
 
+    String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_profile);
-        final EditText name=findViewById(R.id.Name);
-        final EditText email=findViewById(R.id.Email);
-        final EditText gender=findViewById(R.id.Gender);
-        final EditText dob=findViewById(R.id.Dob);
-        final EditText mob=findViewById(R.id.editText);
-        final EditText add=findViewById(R.id.editText2);
+        final TextView name=findViewById(R.id.Name);
+        final TextView email=findViewById(R.id.Email);
+        final TextView gender=findViewById(R.id.Gender);
+        final TextView dob=findViewById(R.id.Dob);
+        final TextView mob=findViewById(R.id.editText);
+        final TextView add=findViewById(R.id.editText2);
+        final ImageView image = findViewById(R.id.photo);
         Button b=(Button) findViewById(R.id.button);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,18 +41,24 @@ public class display_profile extends AppCompatActivity {
             }
         });
         FirebaseAuth mAuth=FirebaseAuth.getInstance();
-        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Users").child(mAuth.getCurrentUser().getUid());
+        String uid = mAuth.getCurrentUser().getUid();
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Users").child(uid);
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                {
                 UserAK us=dataSnapshot.getValue(UserAK.class);
+                while(us==null);
                 name.setText(us.Name);
                 email.setText(us.Email);
                 gender.setText(us.Gender);
                 dob.setText(us.Date_of_Birth);
                 mob.setText(us.Mob);
                 add.setText(us.Add);
-            }
+                url = us.image;
+                Glide.with(getApplicationContext()).load(url).into(image);
+            }}
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
